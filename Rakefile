456bfaf74d8afaca3ac0fcd2c3a6ba46045ceb84
Apprentice <apprentice@dbc09.local>
2013-10-29 00:23:25 -0700

    "Create Read Update of CRUD method"

--- Rakefile
+++ Rakefile
@@ -4,12 +4,12 @@ require './app'
 namespace :db do
   desc "create the postgres database"
   task :create do
-    `createdb habits_db`
+    `createdb habit_db`
   end
 
   desc "drop the postgres database"
   task :drop do
-    `dropdb habits_db`
+    `dropdb habit_db`
   end
 end
 
--- app.rb
+++ app.rb
@@ -1,8 +1,42 @@
 require 'rubygems'
+require 'active_record'
 require 'sinatra'
+require_relative './app/models/habit.rb'
 
 
-get '/index' do 
- erb :form 
+ActiveRecord::Base.establish_connection(adapter: 'postgresql', database: 'habit_db')
+
+
+
+get '/' do 
+  erb :index
+end 
+
+post '/' do 
+  habit = Habit.new 
+  habit.habit_name = params[:name]
+  habit.habit_description = params[:content]
+  habit.created_at = Time.now
+  habit.updated_at = Time.now 
+  habit.save
+  erb :index 
+end 
+
+
+get '/read' do 
+    @habit = Habit.all   
+    erb :read
+end 
+
+get '/habit/:id' do 
+  @habit = Habit.find(params[:id])
+  erb :form
+end 
+
+post '/update/:id' do 
+ @habit = Habit.find(params[:id]) 
+  @habit.habit_name = params[:name]
+  @habit.save 
+  erb :index 
 end 
 
